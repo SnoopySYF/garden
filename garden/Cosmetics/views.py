f04cfdd18baad8d18580d8e2733716ea251bf050
SnoopySYF <1064632198@qq.com>
2020-08-22 13:32:10 +0800

    接口

--- garden/Cosmetics/views.py
+++ garden/Cosmetics/views.py
@@ -6,6 +6,8 @@ from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+from garden_python.cosmetics import Cosmetics
+from garden_python.mysql import GMysql
 
 # Create your views here.
 def imgUpload(request):
@@ -30,4 +32,19 @@ def Cosmetics_Search(request, search):
 
 def test(request):
     print("test res")
-    return JsonResponse({'status': 'OK'})
+    Lipstick_color_D = Cosmetics().Lipstick_color_D()   #色号识别
+    Lipstick_color_recommend = Cosmetics().Lipstick_color_recommend()  #色号推荐
+    select_brands = GMysql().select_brands()  #查询所有的品牌名
+    select_series = GMysql().select_series()  #查询某个品牌名下的所有系列
+    select_lipsticks = GMysql().select_lipsticks() #查询某个品牌名下某个系列的所有色号
+    data = {
+        'msg' : 'success',
+        'data': {
+            'Lipstick_color_D': Lipstick_color_D,
+            'Lipstick_color_recommend': Lipstick_color_recommend,
+            'select_brands': select_brands,
+            'select_series': select_series,
+            'select_lipsticks': select_lipsticks
+        }
+    }
+    return JsonResponse(data)
